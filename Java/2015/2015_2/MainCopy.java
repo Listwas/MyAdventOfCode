965d5038ae6ab65e54c0fde4bb20c71b0d175974
listwas <listwa.patrykkr@gmail.com>
2025-03-31 22:48:18 +0200

    task 2 part 1 in progress

--- Java/2015/2015_2/MainCopy.java
+++ Java/2015/2015_2/MainCopy.java
@@ -1,27 +1,42 @@
 package com.github.listwas;
-import java.io.File;
-import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            // read and open file
-            File file = new File("input.txt");
-            FileInputStream fis = new FileInputStream(file);
+            // split input string by 'x' character
+            String fCon = new String(Files.readAllBytes(Paths.get("input.txt")));
 
-            // get length
-            int fLen = (int) file.length();
+            String regex = "[x\n]";
+            String[] split = fCon.split(regex);
 
-            // save input to string array
-            String[] fCon = new String[fLen];
-            String string = "test string";
-            String[] fSplit = string.split("x");
-            for (String s : fSplit) {
-                System.out.println(s);
-            }
+            int res = getRes(split);
+            System.out.println(res);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static int getRes(String[] split) {
+        int fLen = split.length;
+        int length, width, height, res = 0;
+
+        for (int i = 0; i < fLen-2; i += 3) {
+            length = Integer.parseInt(split[i]);
+            width = Integer.parseInt(split[i+1]);
+            height = Integer.parseInt(split[i+2]);
+
+            // TODO
+            // DETERMINATE SMALLEST NUMBER FOR SLACK
+
+            res += 2*(length * width + width * height + height * length);
+//                System.out.println(split[i]);
+//                System.out.printf("%d %d %d\n", length, width, height);
+//                System.out.println(res);
+        }
+        return res;
     }
 }
