c5ba2e113853c91ddeacccbebb5b9d62c1f04d56
listwas <listwa.patrykkr@gmail.com>
2025-04-01 16:43:58 +0200

    task 2 part 1 solved

--- Java/2015/2015_2/MainCopy.java
+++ Java/2015/2015_2/MainCopy.java
@@ -1,42 +1,42 @@
 package com.github.listwas;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            // split input string by 'x' character
+            // read file and then split it by 'x\n' characters
             String fCon = new String(Files.readAllBytes(Paths.get("input.txt")));
+            String[] split = fCon.split("[x\n]");
+            int fLen = split.length;
 
-            String regex = "[x\n]";
-            String[] split = fCon.split(regex);
+            // get l, w, h for further calculations
+            int totalWrap = 0;
+            for (int i = 0; i < fLen; i += 3) {
+                int length = Integer.parseInt(split[i]);
+                int width = Integer.parseInt(split[i + 1]);
+                int height = Integer.parseInt(split[i + 2]);
 
-            int res = getRes(split);
-            System.out.println(res);
+                totalWrap += getRes(length, width, height);
+            }
+
+            System.out.println("total square feet of wrapping paper: " + totalWrap);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+    private static int getRes(int length, int width, int height) {
+        // calculate surface area
+        int surfaceArea = 2 * (length * width + width * height + height * length);
 
-    private static int getRes(String[] split) {
-        int fLen = split.length;
-        int length, width, height, res = 0;
+        // calculate the area of the smallest side
+        int area1 = length * width;
+        int area2 = width * height;
+        int area3 = height * length;
+        int smallestSide = Math.min(Math.min(area1, area2), area3);
 
-        for (int i = 0; i < fLen-2; i += 3) {
-            length = Integer.parseInt(split[i]);
-            width = Integer.parseInt(split[i+1]);
-            height = Integer.parseInt(split[i+2]);
-
-            // TODO
-            // DETERMINATE SMALLEST NUMBER FOR SLACK
-
-            res += 2*(length * width + width * height + height * length);
-//                System.out.println(split[i]);
-//                System.out.printf("%d %d %d\n", length, width, height);
-//                System.out.println(res);
-        }
-        return res;
+        return surfaceArea + smallestSide;
     }
+
 }
